d5f8edf0cc8c86e0cabde5856ec04c7b165abd60
Masumori <masumori.toshiaki@sysmex.co.jp>
2025-09-03 00:20:41 +0900

    add generator

--- src/generator/__init__.py
+++ src/generator/__init__.py
@@ -1,4 +1,16 @@
 import os
+from generator.prompt import GeneratePromptBuilder
+from generator.caller import LlmCaller
 
 if "GOOGLE_APPLICATION_CREDENTIALS" not in os.environ:
     raise ValueError("GOOGLE_APPLICATION_CREDENTIALS environment variable not set")
+
+
+class DocumentGenerator:
+    def __init__(self, code_snippet: str):
+        self.code_snippet = code_snippet
+
+    def generate(self, prompt: str) -> str:
+        prompt = GeneratePromptBuilder().build(self.code_snippet)
+        response = LlmCaller().call(prompt)
+        return response
